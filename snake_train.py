b62909a83a092620e20f6af5ad38776968892507
JonnasMaravilla <jonathan.arreola@outlook.com>
2020-03-18 17:57:34 -0600

    refactored snake_game.py, small change in snake_train.py

--- snake_train.py
+++ snake_train.py
@@ -7,9 +7,9 @@ from snake_game import SnakeGame
 
 
 def train(iters, warm_start=False, verbose=False, learning_rate=0.8, gamma=0.8, epsilon=0.2,
-          dont_repeat=False):
+          dont_repeat=False, name="memory.pkl"):
     if warm_start:
-        ai = joblib.load("memory.pkl")
+        ai = joblib.load(name)
     else:
         ai = QLearn([0, 1, 2, 3])
     ai.learning_rate = learning_rate
@@ -21,14 +21,14 @@ def train(iters, warm_start=False, verbose=False, learning_rate=0.8, gamma=0.8, 
     bu_iter = 100
     for i in range(1, iters + 1):
         game = SnakeGame()
-        ai = game.demo_light(ai)
+        ai = game.demo(ai, light_mode=True)
         evals.append(np.sum(np.array([v for v in ai.memory.values()])))
         plt.plot(evals, c="b")
         plt.pause(0.05)
         if not i % bu_iter:
-            joblib.dump(ai, "memory.pkl")
-    joblib.dump(ai, "memory.pkl")
+            joblib.dump(ai, name)
+    joblib.dump(ai, name)
 
 
 if __name__ == '__main__':
-    train(2000, epsilon=0.8, dont_repeat=True, warm_start=False)
+    train(2000, epsilon=0.8, dont_repeat=True, warm_start=True, name="ignore_test")
